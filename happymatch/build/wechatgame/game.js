daba9a8e290171cd4fa1707e6e411368f2b5d269
LJKOJBK <39849103+LJKOJBK@users.noreply.github.com>
2020-01-07 22:37:04 +0800

    构建发布

--- happymatch/build/wechatgame/game.js
+++ happymatch/build/wechatgame/game.js
@@ -2,9 +2,9 @@ require('libs/weapp-adapter/index');
 var Parser = require('libs/xmldom/dom-parser');
 window.DOMParser = Parser.DOMParser;
 require('libs/wx-downloader.js');
-require('src/settings.9bf46');
+require('src/settings.8ab9e');
 var settings = window._CCSettings;
-require('main.573a9');
+require('main.a7e27');
 
 // Will be replaced with cocos2d-js path in editor
 require('cocos/cocos2d-js-min.js');
